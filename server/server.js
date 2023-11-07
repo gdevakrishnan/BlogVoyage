099e6bd6a4992911edb0afbfb8f1a07e2b038315
gdevakrishnan <gdevakrishnan700@gmail.com>
2023-11-08 00:15:29 +0530

    Project Setup

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const { default: mongoose } = require('mongoose');
+
+require('dotenv').config();
+const { PORT, MONGO_URI } = process.env;
+
+app.use(express.json({extended: false}));
+app.use(cors({origin: true, Credential: true}));
+
+mongoose.connect(MONGO_URI)
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`The Database was Connected Successfully\nThe Server was Listening\nhttp://localhost:${PORT}`);
+        });
+    })
+    .catch((e) => console.log(e.message));
