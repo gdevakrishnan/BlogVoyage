a9a9c817cb122fffdb5ae900f4111056c56850e4
gdevakrishnan <gdevakrishnan700@gmail.com>
2023-11-20 01:10:24 +0530

    Read Update Blog BlogTitle

--- client/src/components/Posts.jsx
+++ client/src/components/Posts.jsx
@@ -6,7 +6,7 @@ import NoDataFound from './NoDataFound';
 import { useNavigate } from 'react-router-dom';
 
 function Posts() {
-    const { userDetails, setMsg, editBlogDetails, setEditBlogDetails } = useContext(userContext);
+    const { userDetails, setMsg, setABlogDetails, setEditBlogDetails } = useContext(userContext);
     const { _id } = userDetails;
     const [blogs, setBlogs] = useState(null);
     const BASE_URL = "http://localhost:5000/public/thumbnails/";
@@ -19,6 +19,18 @@ function Posts() {
             })
             .catch((e) => console.log(e.message));
     })
+  
+    const viewBlog = (aBlog) => {
+        setABlogDetails(aBlog);
+        nav('/blog');
+        window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+    }
+
+    const handleEditBtn = (blogDetails) => {
+        setEditBlogDetails(blogDetails);
+        nav('/edit');
+        window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+    }
 
     const handleDelete = (blogDetails) => {
         deletePost(blogDetails)
@@ -30,17 +42,11 @@ function Posts() {
             .catch((e) => console.log(e.message));
     }
 
-    const handleEditBtn = (blogDetails) => {
-        setEditBlogDetails(blogDetails);
-        nav('/edit');
-        window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
-    }
-
     return (
         <Fragment>
             {
                 (blogs) ? (
-                    <section className='page blog_page'>
+                    <section className='page blog_page blog_post_page'>
                         {
                             (blogs.length >= 1) ? (
                                 blogs.map((aBlog, index) => {
@@ -51,13 +57,16 @@ function Posts() {
                                             </div>
                                             <div className="content">
                                                 <div className="sample">
-                                                    <h1 className="blogTitle">{aBlog.blogTitle}</h1>
+                                                    <h1 className="blogTitle" title={aBlog.blogTitle} >{aBlog.blogTitle}</h1>
                                                     <p className='blogAuthor'>Author: {aBlog.uname}</p>
                                                     <p className="blog">{aBlog.blog.slice(0, 150)}...</p>
                                                 </div>
                                                 <div className="btn">
+                                                    <button className="readBtn" onClick={() => viewBlog(aBlog)}><i className='fa fa-eye'></i></button>
+
                                                     <button className="editBtn" onClick={() => handleEditBtn(aBlog)}><i className='fa fa-pencil'></i></button>
-                                                    <button className="readBtn" onClick={() => handleDelete(aBlog)}>DELETE <i className='fa fa-bitbucket'></i></button>
+
+                                                    <button className="deleteBtn" onClick={() => handleDelete(aBlog)}>DELETE <i className='fa fa-bitbucket'></i></button>
                                                 </div>
                                             </div>
                                         </div>
