41033ea4f731e7a7d266cc9abd60ac0c9751f45a
gdevakrishnan <gdevakrishnan700@gmail.com>
2024-06-28 13:11:07 +0530

    deploy client side

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,11 +3,11 @@ import React, { Fragment } from 'react'
 function Footer() {
   return (
     <Fragment>
-        <footer>
-            <h1 className="footer_content">
-                Blog Voyage &copy; 2023 | Sigma Software Solutions | <span>House Of Metazord</span>
-            </h1>
-        </footer>
+      <footer>
+        <h1 className="footer_content">
+          <span>Blog Voyage &copy; 2023 | Sigma Software Solutions | House Of Metazord</span>
+        </h1>
+      </footer>
     </Fragment>
   )
 }
